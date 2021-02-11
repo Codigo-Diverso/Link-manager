32c6d815e02176ec176c2e1f3ddb633130c1044f
carlosalfarodev <rgkalin@gmail.com>
2021-02-11 13:52:28 -0800

    setup testing routes with express

--- server/server.js
+++ server/server.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Link-manager");
+  res.send("Link-manager API is running");
+});
+
+app.get("/api/resources", (req, res) => {
+  res.send("Fetching all resources");
+});
+
+app.get("api/resources/:id", (req, res) => {
+  res.send("Fetching a resource");
 });
 
 app.listen(port, () => {
