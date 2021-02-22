ae14368a0c0ef972fd0aaea78ff3c01d2b69092d
carlosalfarodev <rgkalin@gmail.com>
2021-02-22 07:24:14 -0800

    Adds selection and sorting

--- server/controllers/web-links.js
+++ server/controllers/web-links.js
@@ -6,7 +6,43 @@ const WebLink = require("../models/WebLink");
 // @route   GET /api/v1/web-links
 // @access  Public
 exports.getWebLinks = asyncHandler(async (req, res, next) => {
-  const webLinks = await WebLink.find();
+  let query;
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ["select", "sort"];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => {
+    return `$${match}`;
+  });
+
+  // Finding resource
+  query = WebLink.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Excecuting query
+  const webLinks = await query;
   res.status(200).json({
     success: true,
     count: webLinks.length,
